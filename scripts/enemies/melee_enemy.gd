59dd2c30985e1ea32c9c83ded8e934ba9e106ed1
Cédric Lesueur <cedric_lesueur@outlook.fr>
2024-06-05 22:07:11 +0200

    Updated enemy movement

--- scripts/enemies/melee_enemy.gd
+++ scripts/enemies/melee_enemy.gd
@@ -7,8 +7,19 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float):
-	pass
+func _process(delta: float):
+	if _target != null && state_machine.can_move():
+			var direction_to_target = (_target.position - global_position).normalized()
+			move_and_collide(direction_to_target * _speed * delta)
 
 func attack():
 	_target.take_damage(_attack_damage)
+
+func take_damage(damage: float):
+	_hp -= damage
+	print(name + " has " + str(_hp) + "hp left")
+	if _hp <= 0:
+		_die()
+
+func _die():
+	queue_free()
